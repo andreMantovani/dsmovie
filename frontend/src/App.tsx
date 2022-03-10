33792c01782ff3e5d1ec1d207ab523f4d5e80781
Andre Mantovani <amantovani85@gmail.com>
2022-03-09 21:07:14 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
