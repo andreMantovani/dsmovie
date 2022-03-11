524bba1d930f2937b3eeb15a05277210c16dd7ad
Andre Mantovani <amantovani85@gmail.com>
2022-03-11 16:58:04 -0300

    Props

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,17 +1,22 @@
 import MovieStars from 'components/MovieStars';
+import { Movie } from 'types/movie';
 import './styles.css';
 
-function MovieScore() {
+type Props = {
+  movie: Movie;
+}
 
-  const score = 3.5;
-  const count = 13;
+function MovieScore({ movie } : Props) {
+
+  const score = movie.score;
+  const count = movie.count;
 
   return (
     <div className="dsmovie-score-container">
-    <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-    <MovieStars />
-    <p className="dsmovie-score-count">{count} avaliações</p>
-</div>
+      <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+      <MovieStars />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
   );
 }
 
